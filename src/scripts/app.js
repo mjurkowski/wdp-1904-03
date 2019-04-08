7e37e1bdf50283c562da5a5f9f7d6f407dd7ecfd
Rafal Pijet <stronglopez@wp.pl>
2019-04-08 20:35:32 +0200

    Fixed bug-WDP190403-2

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,8 @@
 console.log('hello');
+
+const cartCounter = value => {
+  const element = document.getElementById('cart-counter');
+  value >= 0 && value <= 99999 ? (element.innerText = value) : (element.innerText = 0);
+};
+
+cartCounter(123);
