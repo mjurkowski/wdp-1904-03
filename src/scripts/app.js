f9458709ce3b8523eeac26fb524e172f28bade4f
Marcin Polonis <marcin.polonis@o2.pl>
2019-04-14 14:01:35 +0200

    Add RWD for major menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,17 @@
 console.log('hello');
+
+// toggle-menu on mobile
+
+var toggleSidebar = function () {
+  var menuMobile = document.querySelector('#toggle-menu');
+  var sidebarMenu = document.querySelector('#sidebar-mobile');
+
+  menuMobile.classList.toggle('active');
+  sidebarMenu.classList.toggle('hidden');
+};
+
+var hamburgerMobile = document.querySelector('#hamburger-on-mobile');
+var hamburgerToggle = document.querySelector('#hamburger-toggle');
+
+hamburgerMobile.addEventListener('click', toggleSidebar);
+hamburgerToggle.addEventListener('click', toggleSidebar);
