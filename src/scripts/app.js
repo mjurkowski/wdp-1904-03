1f6391f559e1476a5e1af4aff947e67d311e495f
Rafal Pijet <stronglopez@wp.pl>
2019-04-18 22:35:13 +0200

    Correcting my solution of task bug-WDP190403-2

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,1 @@
 console.log('hello');
-
-const cartCounter = value => {
-  const element = document.getElementById('cart-counter');
-  value >= 0 && value <= 99999 ? (element.innerText = value) : (element.innerText = 0);
-};
-
-cartCounter(123);
