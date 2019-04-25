526ea01188358ad3093095ba8c9b73897421fa7b
Marcin Polonis <marcin.polonis@o2.pl>
2019-04-25 16:26:33 +0200

    Start adding star rating system

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,3 +12,48 @@ var hamburgerToggle = document.querySelector('#hamburger-toggle');
 
 hamburgerMobile.addEventListener('click', toggleSidebar);
 hamburgerToggle.addEventListener('click', toggleSidebar);
+
+//  star
+
+const productRating = document.getElementById('product-rating');
+const stars = productRating.querySelectorAll('.star');
+let rating = 0;
+
+productRating.addEventListener('click', e => {
+  if (!e.target.matches('.star')) return;
+  e.preventDefault();
+
+  const starID = parseInt(e.target.getAttribute('data-star'));
+
+  removeClassFromElements('is-active', stars);
+  highlightStars(starID);
+
+  rating = starID;
+});
+
+productRating.addEventListener('mouseover', e => {
+  if (!e.target.matches('.star')) return;
+
+  removeClassFromElements('is-active', stars);
+  const starID = parseInt(e.target.getAttribute('data-star'));
+  highlightStars(starID);
+});
+
+productRating.addEventListener('mouseleave', e => {
+  removeClassFromElements('is-active', stars);
+  if (rating === 0) return;
+  highlightStars(rating);
+});
+
+function highlightStars (starID) {
+  for (let i = 0; i < starID; i++) {
+    stars[i].classList.add('is-active');
+  }
+}
+
+function removeClassFromElements (className, elements) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.remove(className);
+  }
+}
+//
