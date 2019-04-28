8c614760596c95136260a10ba54cc7d6be5f866b
Tomasz Rusin <tomek@rusin.info>
2019-04-28 19:06:40 +0200

    Add working carousel

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,7 @@ console.log('hello');
 
 // toggle-menu on mobile
 
-var toggleSidebar = function () {
+var toggleSidebar = function() {
   var menuMobile = document.querySelector('#toggle-menu');
   menuMobile.classList.toggle('active');
 };
@@ -12,3 +12,40 @@ var hamburgerToggle = document.querySelector('#hamburger-toggle');
 
 hamburgerMobile.addEventListener('click', toggleSidebar);
 hamburgerToggle.addEventListener('click', toggleSidebar);
+
+// Enable carousel
+document.addEventListener('DOMContentLoaded', initCarousel);
+
+function initCarousel() {
+  let queryXL = window.matchMedia('(min-width: 1200px)');
+
+  let queryL = window.matchMedia('(min-width: 993px) and (max-width: 1199px)');
+
+  let queryM = window.matchMedia('(min-width:767px) and (max-width: 992px)');
+
+  let queryS = window.matchMedia('(max-width: 766px)');
+
+  if (queryS.matches) {
+    var flkty = new Flickity('.main-carousel', {
+      groupCells: 1
+    });
+  }
+
+  if (queryM.matches) {
+    var flkty = new Flickity('.main-carousel', {
+      groupCells: 2
+    });
+  }
+
+  if (queryL.matches) {
+    var flkty = new Flickity('.main-carousel', {
+      groupCells: 3
+    });
+  }
+
+  if (queryXL.matches) {
+    var flkty = new Flickity('.main-carousel', {
+      groupCells: 4
+    });
+  }
+}
