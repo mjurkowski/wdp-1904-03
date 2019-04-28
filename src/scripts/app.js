a5c9d368de44ed7f4866d8c7e6d8a9ffe494389d
Rafal Pijet <stronglopez@wp.pl>
2019-04-28 17:11:55 +0200

    Change solution after MaciejKut sugestion

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,13 @@
 import Flickity from 'flickity';
 
-const pointsBox = document.querySelector('.points-box');
-const firstPoint = document.getElementById('first-point');
-const secondPoint = document.getElementById('second-point');
-const thirdPoint = document.getElementById('third-point');
 const carouselCells = document.querySelectorAll('.images');
 const leftArrow = document.querySelector('.arrow-left');
 const rightArrow = document.querySelector('.arrow-right');
 
 const leftCarousel = new Flickity('.hot-deals-carousel', {
-  pageDots: false,
+  pageDots: true,
   wrapAround: true,
-  autoPlay: 3000,
+  autoPlay: true,
   prevNextButtons: false,
   draggable: false
 });
@@ -23,52 +19,12 @@ const rightCarousel = new Flickity('.products-carousel', {
   draggable: false
 });
 
-const activeFirstPoint = () => {
-  firstPoint.classList.add('active');
-  secondPoint.classList.remove('active');
-  thirdPoint.classList.remove('active');
-};
+const pointsBox = document.querySelectorAll('.section--promoted .hot-deals li');
 
-const activeSecondPoint = () => {
-  secondPoint.classList.add('active');
-  firstPoint.classList.remove('active');
-  thirdPoint.classList.remove('active');
-};
-
-const activeThirdPoint = () => {
-  thirdPoint.classList.add('active');
-  firstPoint.classList.remove('active');
-  secondPoint.classList.remove('active');
-};
-
-firstPoint.addEventListener('click', () => {
-  leftCarousel.selectCell(0);
-  leftCarousel.pausePlayer();
-  activeFirstPoint();
-});
-
-secondPoint.addEventListener('click', () => {
-  leftCarousel.selectCell(1);
-  leftCarousel.pausePlayer();
-  activeSecondPoint();
-});
-
-thirdPoint.addEventListener('click', () => {
-  leftCarousel.selectCell(2);
-  leftCarousel.pausePlayer();
-  activeThirdPoint();
-});
-
-pointsBox.addEventListener('mouseleave', () => leftCarousel.unpausePlayer());
-
-leftCarousel.on('change', () => {
-  if (leftCarousel.selectedIndex === 0) {
-    activeFirstPoint();
-  } else if (leftCarousel.selectedIndex === 1) {
-    activeSecondPoint();
-  } else {
-    activeThirdPoint();
-  }
+pointsBox.forEach(el => {
+  el.addEventListener('mouseleave', () => {
+    leftCarousel.playPlayer();
+  });
 });
 
 carouselCells.forEach(cell =>
