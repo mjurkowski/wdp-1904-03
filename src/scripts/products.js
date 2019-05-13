ad8f476c84c29c39294e295ca70bde96d712ab75
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-05-14 00:46:16 +0200

    Change product.js and added dots to carousel

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -1,41 +1,46 @@
+/* eslint-disable space-before-function-paren */
 import Flickity from 'flickity';
 
-const firstDot = document.querySelector('.dotF');
-const secondDot = document.querySelector('.dotS');
-const thirdDot = document.querySelector('.dotT');
-var productCarousel = new Flickity('.carousel-1', {
-  pageDots: false,
-  prevNextButtons: false,
-  setGallerySize: false
-});
-window.addEventListener('resize', () => {
-  productCarousel.options.draggable = window.innerWidth < 992;
-  productCarousel.updateDraggable();
-});
-firstDot.addEventListener('click', e => {
-  productCarousel.select(0);
-  productCarousel2.select(0);
-  firstDot.classList.add('active');
-  secondDot.classList.remove('active');
-  thirdDot.classList.remove('active');
-});
-secondDot.addEventListener('click', () => {
-  productCarousel.select(1);
-  productCarousel2.select(1);
-  secondDot.classList.add('active');
-  firstDot.classList.remove('active');
-  thirdDot.classList.remove('active');
-});
-thirdDot.addEventListener('click', () => {
-  productCarousel.select(2);
-  productCarousel2.select(2);
-  thirdDot.classList.add('active');
-  firstDot.classList.remove('active');
-  secondDot.classList.remove('active');
+const tabs = document.querySelectorAll('.nav-item');
+const tabsArray = [...tabs];
+
+var initialized = null;
+
+tabsArray.forEach(tab => {
+  tab.addEventListener('click', e => {
+    const id = `carousel-${e.target.id}`;
+    if (initialized) {
+      initialized.destroy();
+    }
+    initialized = new Flickity(`#${id}`, {
+      pageDots: false,
+      prevNextButtons: false,
+      setGallerySize: false
+    });
+    registerDotListeners();
+  });
 });
 
-var productCarousel2 = new Flickity('.carousel-2', {
-  pageDots: false,
-  prevNextButtons: false,
-  setGallerySize: false
-});
+function registerDotListeners() {
+  const firstDot = document.querySelector('.dot-first');
+  const secondDot = document.querySelector('.dot-second');
+  const thirdDot = document.querySelector('.dot-third');
+  firstDot.addEventListener('click', e => {
+    initialized.select(0);
+    firstDot.classList.add('active');
+    secondDot.classList.remove('active');
+    thirdDot.classList.remove('active');
+  });
+  secondDot.addEventListener('click', () => {
+    initialized.select(1);
+    secondDot.classList.add('active');
+    firstDot.classList.remove('active');
+    thirdDot.classList.remove('active');
+  });
+  thirdDot.addEventListener('click', () => {
+    initialized.select(2);
+    thirdDot.classList.add('active');
+    firstDot.classList.remove('active');
+    secondDot.classList.remove('active');
+  });
+}
