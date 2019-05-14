a26b7d2d50bc0e5480316a9f7d7b9752c936cacc
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-05-14 16:14:29 +0200

    Added code to product.js with register position of slides in carrousel & dots

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -1,25 +1,68 @@
 /* eslint-disable space-before-function-paren */
 import Flickity from 'flickity';
 
-const tabs = document.querySelectorAll('.nav-item');
-const tabsArray = [...tabs];
-
 var initialized = null;
+initialize();
 
-tabsArray.forEach(tab => {
-  tab.addEventListener('click', e => {
-    const id = `carousel-${e.target.id}`;
-    if (initialized) {
-      initialized.destroy();
-    }
-    initialized = new Flickity(`#${id}`, {
-      pageDots: false,
-      prevNextButtons: false,
-      setGallerySize: false
-    });
-    registerDotListeners();
+function initialize() {
+  registerListeners();
+}
+
+function registerListeners() {
+  registerDotListeners();
+  registerTabListeners();
+}
+
+function reinitialize(e) {
+  const id = `carousel-${e.target.id}`;
+  if (initialized) {
+    initialized.destroy();
+  }
+  initialized = new Flickity(`#${id}`, {
+    pageDots: false,
+    prevNextButtons: false,
+    setGallerySize: false
   });
-});
+}
+
+function registerTabListeners() {
+  const tabs = document.querySelectorAll('.nav-item');
+  const tabsArray = [...tabs];
+
+  tabsArray.forEach(tab => {
+    tab.addEventListener('click', e => setTimeout(() => reinitialize(e), 1000));
+  });
+}
+
+function firstDotClassAdd() {
+  const firstDot = document.querySelector('.dot-first');
+  firstDot.classList.add('active');
+}
+
+function secondDotClassAdd() {
+  const secondDot = document.querySelector('.dot-second');
+  secondDot.classList.add('active');
+}
+
+function thirdDotClassAdd() {
+  const thirdDot = document.querySelector('.dot-third');
+  thirdDot.classList.add('active');
+}
+
+function firstDotClassRemove() {
+  const firstDot = document.querySelector('.dot-first');
+  firstDot.classList.remove('active');
+}
+
+function secondDotClassRemove() {
+  const secondDot = document.querySelector('.dot-second');
+  secondDot.classList.remove('active');
+}
+
+function thirdDotClassRemove() {
+  const thirdDot = document.querySelector('.dot-third');
+  thirdDot.classList.remove('active');
+}
 
 function registerDotListeners() {
   const firstDot = document.querySelector('.dot-first');
@@ -27,20 +70,20 @@ function registerDotListeners() {
   const thirdDot = document.querySelector('.dot-third');
   firstDot.addEventListener('click', e => {
     initialized.select(0);
-    firstDot.classList.add('active');
-    secondDot.classList.remove('active');
-    thirdDot.classList.remove('active');
+    firstDotClassAdd();
+    secondDotClassRemove();
+    thirdDotClassRemove();
   });
   secondDot.addEventListener('click', () => {
     initialized.select(1);
-    secondDot.classList.add('active');
-    firstDot.classList.remove('active');
-    thirdDot.classList.remove('active');
+    secondDotClassAdd();
+    firstDotClassRemove();
+    thirdDotClassRemove();
   });
   thirdDot.addEventListener('click', () => {
     initialized.select(2);
-    thirdDot.classList.add('active');
-    firstDot.classList.remove('active');
-    secondDot.classList.remove('active');
+    thirdDotClassAdd();
+    firstDotClassRemove();
+    secondDotClassRemove();
   });
 }
