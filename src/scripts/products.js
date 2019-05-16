e947644f33bd7170b9338c829dfa98fa7030da47
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-05-16 10:54:08 +0200

    new code in product.js

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -34,56 +34,15 @@ function registerTabListeners() {
   });
 }
 
-function firstDotClassAdd() {
-  const firstDot = document.querySelector('.dot-first');
-  firstDot.classList.add('active');
-}
-
-function secondDotClassAdd() {
-  const secondDot = document.querySelector('.dot-second');
-  secondDot.classList.add('active');
-}
-
-function thirdDotClassAdd() {
-  const thirdDot = document.querySelector('.dot-third');
-  thirdDot.classList.add('active');
-}
-
-function firstDotClassRemove() {
-  const firstDot = document.querySelector('.dot-first');
-  firstDot.classList.remove('active');
-}
-
-function secondDotClassRemove() {
-  const secondDot = document.querySelector('.dot-second');
-  secondDot.classList.remove('active');
-}
-
-function thirdDotClassRemove() {
-  const thirdDot = document.querySelector('.dot-third');
-  thirdDot.classList.remove('active');
-}
-
 function registerDotListeners() {
-  const firstDot = document.querySelector('.dot-first');
-  const secondDot = document.querySelector('.dot-second');
-  const thirdDot = document.querySelector('.dot-third');
-  firstDot.addEventListener('click', e => {
-    initialized.select(0);
-    firstDotClassAdd();
-    secondDotClassRemove();
-    thirdDotClassRemove();
+  const navAnchor = document.querySelectorAll('.dot');
+  navAnchor.forEach(anchor => {
+    anchor.addEventListener('click', changeClassActive);
   });
-  secondDot.addEventListener('click', () => {
-    initialized.select(1);
-    secondDotClassAdd();
-    firstDotClassRemove();
-    thirdDotClassRemove();
-  });
-  thirdDot.addEventListener('click', () => {
-    initialized.select(2);
-    thirdDotClassAdd();
-    firstDotClassRemove();
-    secondDotClassRemove();
-  });
+}
+
+function changeClassActive(e) {
+  const current = document.querySelector('.activer');
+  current.className = current.className.replace(' activer', '');
+  e.target.className += ' activer';
 }
